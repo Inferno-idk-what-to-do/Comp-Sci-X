6c9371798a9f3ae85a1b9c59310435fd17f60700
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2022-09-12 17:00:43 -0400

    cleannn

--- isbn/src/main.rs
+++ isbn/src/main.rs
@@ -1,9 +1,7 @@
 // this has to be one of the worst remakes of this program but it works
 
-use std::convert::TryInto;
-
 fn main() {
-    let isbn: String = String::from("0-7645-0417-7");
+    let isbn: String = String::from("0-912517-31-X");
     let check_digit: char = isbn.chars().last().unwrap();
 
     println!("ISBN Number -> {}", isbn);
@@ -12,18 +10,18 @@ fn main() {
 }
 
 fn calc_check_digit(isbn: &String) -> char {
-    let mut isbn_clean: String = String::from(isbn.replace("-", ""));
+    let mut isbn_clean = String::from(isbn.replace("-", ""));
     isbn_clean.pop();
 
-    let mut mult = 10;
+    let mult = 10;
     let mut sum = 0;
-    for c in isbn_clean.chars() {
-        sum += mult * (c as u32 - 0x30);
-        mult -= 1;
+
+    for i in 0..=8 {
+        sum += (mult - i) * (isbn_clean.chars().nth(i as usize).unwrap() as u32 - 0x30)
     }
 
     let digit = 10 - ((sum - 1) % 11);
-    let digit_char = "0123456789X".chars().nth(digit.try_into().unwrap()).unwrap();
+    let digit_char = "0123456789X".chars().nth(digit as usize).unwrap();
 
     return digit_char;
 }
