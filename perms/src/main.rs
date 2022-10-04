5d87f0186edf724ca71b47dbcb81678bed224eb1
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2022-10-04 11:07:55 -0400

    all stolen code; just a ref

---
+++ perms/src/main.rs
@@ -0,0 +1,8 @@
+use itertools::Itertools;
+
+fn main() {
+    let items = vec![0, 0, 1, 2];
+    for perm in items.iter().permutations(items.len()).unique() {
+        println!("{:?}", perm);
+    }
+}
